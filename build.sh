519db02e696b7780b3ec0bc0af8adc22b559345d
T.Fukushima <tk44fk40@gmail.com>
2026-07-02 00:35:35 +0900

    feat: compose.sample.yml修正、build.sh改善
    - compose.sample.yml
      - Bazzite側環境特性（X11非対応）対策
      - nvidia_icd_egl.jsonを追加
    - build.sh
      - 先にコンテナ停止
      - 引数をコマンドラインから引き継ぐ
      - ビルド後に builder prune

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-sudo $(which podman-compose) build --no-cache 2>&1 | tee build.log
+sudo $(which podman-compose) down
+sudo $(which podman-compose) build "$@" 2>&1 | tee build.log
+sudo podman builder prune
